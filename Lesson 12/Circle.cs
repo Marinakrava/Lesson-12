45da9dc6c7600e10cf0ec59ddc01e2b716e3f237
yxa.kravchenko@yandex.ru <yxa.kravchenko@yandex.ru>
2022-10-29 12:37:44 +0300

    Добавьте файлы проекта.

---
+++ Lesson 12/Circle.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_12
+{
+    static class Circle
+    {
+        static public double GetSquare(double r)
+        {
+            return Math.Pow(r, 2) * Math.PI;
+        }
+        static public double GetLength(double r)
+        {
+            return 2 * Math.PI * r;
+        }
+        static public string GetCoordinates(double x, double y,double r)
+        {
+            double hypotinuse = Math.Sqrt(Math.Pow((x - 0), 2) + Math.Pow((y - 0), 2));
+            if (hypotinuse <= r)
+                return "Точка принадлежит кругу";
+            else
+            {
+                return "Точка не принадлежит кругу";
+            }
+                
+        }
+    }
+}
---
+++ Lesson 12/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Введите радиус окружности");
+            double r = Convert.ToDouble(Console.ReadLine());
+           
+            double square = Circle.GetSquare(r);
+            Console.WriteLine("Площадь окружности = {0:.00}", square);
+            
+            double length = Circle.GetLength(r);
+            Console.WriteLine("Длина окружности = {0:.00}", length);
+
+            Console.WriteLine("Введите координаты точки");
+            double x = Convert.ToInt32(Console.ReadLine());
+            double y= Convert.ToInt32(Console.ReadLine());        
+            Console.WriteLine(Circle.GetCoordinates(x,y,r));
+
+            Console.ReadKey();
+        }
+    }
+}
